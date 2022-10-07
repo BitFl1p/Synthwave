d3e668a6026e72b8c7dc799f6d4b166df2384118
Ciobanu <K2227668@kingston.ac.uk>
2022-10-07 13:01:54 +0100

    made car move

--- Assets/Code/CarController.cs
+++ Assets/Code/CarController.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CarController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<WheelCollider> wheels;
+    Vector2 input;
+    public float drag;
+    public float speed;
+    public float steer;
+    public void OnMove(InputValue value)
     {
-        
-    }
+        input = value.Get<Vector2>();
 
-    // Update is called once per frame
-    void Update()
+    }
+    private void Update()
     {
-        
+        wheels[0].motorTorque = input.y * speed;
+        wheels[1].motorTorque = input.y * speed;
+        wheels[0].steerAngle = input.x * steer;
+        wheels[1].steerAngle = input.x * steer;
     }
 }
